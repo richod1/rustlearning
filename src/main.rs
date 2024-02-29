0ee2475d5d57fc5d2d13c81ab8bd2cffb89a7538
richod1 <frimpsup@gmail.com>
2024-02-29 11:56:27 +0000

    rust learning

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,32 @@ fn main(){
     
 
     let secret_number=rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is :{secret_number}");
+
+    loop{
     println!("Please input your guess");
     let mut guess=String::new();
 
     io::stdin()
     .read_line(&mut guess)
     .expect("Failed to read line");
-    let guess :u32=guess.trim().parse().expect("Please input a number");
+    // let guess :u32=guess.trim().parse().expect("Please input a number");
+    let guess:u32=match guess.trim().parse(){
+        Ok(num)=>num,
+        Err(_)=>continue,
+    };
     println!("you guessed :{guess}");
 
     match guess.cmp(&secret_number){
         Ordering::Less=>println!("too small"),
         Ordering::Greater=>println!("too big"),
-        Ordering::Equal=>println!("You win"),
+        Ordering::Equal=>{
+            println!("You win!");
+            break;
+        }
     }
+
+    println!("The secret number is :{secret_number}");
+}
 
     
 }
